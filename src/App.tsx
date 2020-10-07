ff5ecd938aeed38c56a2f855df9b8b36781105ae
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2020-10-07 15:01:41 +0200

    Refactored into feature folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react"
 import "./App.css"
-import { HelloWorldButton } from "./HelloWorldButton"
-import { Incrementor } from "./Incrementor"
-import { Ticker } from "./Ticker"
+import { HelloWorldButton } from "features/HelloWorld/HelloWorldButton"
+import { Incrementor } from "features/Incrementor/Incrementor"
+import Ticker from "features/Ticker"
 
 function App() {
   return (
     <div className="App">
       <Incrementor limit={12}/>
       <HelloWorldButton/>
-      <Ticker message="This is a long and scrolling message" size={4}/>
+      <Ticker message="This is a long and scrolling message"  size={4}/>
     </div>
   )
 }
