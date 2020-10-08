cbd29c0db944b3b148f697e3ab6601d47f115584
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2020-10-08 13:47:18 +0200

    EitherAOrB

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import "./App.css"
 import ConnectedTicker from "features/ConnectedTicker"
 import { ComposeExample } from "ComposeExample"
 import BigBorder from "features/BigBorder"
+import EitherAOrB from "features/EitherAOrB"
 
 function App() {
   return (
     <div className="App">
-      <BigBorder>
+      <EitherAOrB a={
+        <BigBorder>
+          <ComposeExample header={<h1>This is the connected ticker</h1>}>
+            <ConnectedTicker/>
+          </ComposeExample>
+        </BigBorder>
+      }
+      b={
         <ComposeExample header={<h1>This is the connected ticker</h1>}>
           <ConnectedTicker/>
         </ComposeExample>
-      </BigBorder>
+      }/>
     </div>
   )
 }
