d2ec46860cc6fc0a4191fad5f743a6aac47340b1
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2020-10-08 16:04:26 +0200

    Routing with links

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import "./App.css"
 import Router from "features/Router"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, NavLink } from "react-router-dom"
 
 function App() {
   return (
     <BrowserRouter>
+      <NavLink to="/" exact>Home</NavLink>
+      <NavLink to="/range/5/50">Range 5-50</NavLink>
       <div className="App">
         <Router/>
       </div>
--- src/features/RouteParams/RouteRangeMinMax.tsx
+++ src/features/RouteParams/RouteRangeMinMax.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react"
-import { useRouteMatch } from "react-router-dom"
+import { useRouteMatch, useHistory } from "react-router-dom"
 import RangeInput from "UI/RangeInput"
 
 export const RouteRangeMinMax = (): JSX.Element => {
 	const { params: { min, max } } = useRouteMatch<{min: string, max: string}>()
 	const [range, setRange] = useState(parseInt(min))
+	const { push } = useHistory()
+
+	const onClick = () => {
+		push("/")
+	}
 
 	return (
 		<>
+			<button onClick={onClick}>Go home</button>
 			<pre><code>{`${min} <= ${range} >= ${max}`}</code></pre>
 			<RangeInput
 			id="router-range"
